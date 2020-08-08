2827183fab8bc73e5f1bb6cf4110ead1d75a1282
Bogdan Vlad <vladbgd@gmail.com>
2020-08-08 13:45:04 +0300

    Add ateliere page.

--- ateliere/views.py
+++ ateliere/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'ateliere.html', {})
+
+def detaliu(request):
+    return render(request, 'ateliere_detaliu.html', {})
